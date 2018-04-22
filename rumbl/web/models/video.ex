47ca6aa5ba7da9e9c6fcb558daeb9ec37a758e4e
cggarcia171 <your.camerongarcia111@gmail.com>
2018-04-22 00:34:19 -0400

    The genre counter works I think

--- rumbl/web/models/video.ex
+++ rumbl/web/models/video.ex
@@ -11,19 +11,21 @@ defmodule Rumbl.Video do
     timestamps()
   end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
-  # def changeset(struct, params \\ %{}) do
-  #   struct
-  #   |> cast(params, [:url, :title, :description])
-  #   |> validate_required([:url, :title, :description])
-  # end
-  @required_fields ~w(url title description)
-  @optional_fields ~w()
-  def changeset(model, params \\ :empty) do
+
+  def changeset(model, params \\ :invalid) do
     model
-    |> cast(params, @required_fields, @optional_fields)
+    |> cast(params, [:url, :title, :description, :category_id])
     |> assoc_constraint(:category)
   end
 end
+
+# @doc """
+# Builds a changeset based on the `struct` and `params`.
+# """
+# def changeset(struct, params \\ %{}) do
+#   struct
+#   |> cast(params, [:url, :title, :description])
+#   |> validate_required([:url, :title, :description])
+# end
+# @required_fields ~w(url title description)
+# @optional_fields ~w()
